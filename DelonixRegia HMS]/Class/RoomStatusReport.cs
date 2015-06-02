a1acd767bce3053f6c400b0f73072619b33a8bf2
BaconIsMyBestFriend <1307247e@student.tp.edu.sg>
2015-06-02 16:12:38 +0800

    roomstatusreport

---
+++ DelonixRegia HMS]/Class/RoomStatusReport.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelonixRegia_HMS_.Class
+{
+    class RoomStatusReport
+    {
+        private string statusID;
+        private string allRoomVacancy;
+        private string allRoomStatus;
+        private string statusDate;
+        private string statusTime;
+
+        public RoomStatusReport(string statusID, string allRoomVacancy, string allRoomStatus, string statusDate, string statusTime)
+        {
+            this.statusID = statusID;
+            this.allRoomVacancy = allRoomVacancy;
+            this.allRoomStatus = allRoomStatus;
+            this.statusDate = statusDate;
+            this.statusTime = statusTime;
+        }
+
+        public string StatusID
+        {
+            get { return statusID; }
+            set { statusID = value; }
+        }
+
+        public string AllRoomVanancy
+        {
+            get { return allRoomVacancy; }
+            set { allRoomVacancy = value; }
+        }
+
+        public string AllRoomStatus
+        {
+            get { return AllRoomStatus; }
+            set { AllRoomStatus = value; }
+        }
+
+        public string StatusDate
+        {
+            get { return statusDate; }
+            set { statusDate = value; }
+        }
+
+        public string StatusTime
+        {
+            get { return statusTime; }
+            set { statusTime = value; }
+        }
+    }
+}
